6dc6ee9e452415cb856c1eefd158431d41618efb
Lionnel Tsuro <lionnel@flatrocksolutions.co.za>
2023-09-19 11:42:41 +0200

    initial

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.flutter_firebase_login
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_firebase_login/app/app.dart';
+import 'package:flutter_firebase_login/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
